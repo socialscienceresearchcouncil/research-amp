9646222e8a4b7a79a38b0e630032ce2e7eebd56a
Boone B Gorges <boonebgorges@gmail.com>
2021-12-03 14:39:47 -0600

    System for shared block styles.

--- assets/src/index.js
+++ assets/src/index.js
@@ -10,6 +10,11 @@ import './blocks/research-topics'
 import './blocks/zotero-library-info-help'
 
 /**
+ * Shared block styles.
+ */
+import '../css/blocks.css'
+
+/**
  * Components
  */
 import { registerPlugin } from '@wordpress/plugins';
