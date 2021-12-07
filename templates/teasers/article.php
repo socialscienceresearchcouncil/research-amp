df542dbb6ff337a567a2eac970bb873f85bfb904
Boone B Gorges <boonebgorges@gmail.com>
2021-12-07 16:23:38 -0600

    Placate PHPCS.

--- templates/teasers/article.php
+++ templates/teasers/article.php
@@ -7,11 +7,11 @@ $img_alt      = '';
 $thumbnail_id = get_post_thumbnail_id( $article );
 if ( $thumbnail_id ) {
 	$all_sizes = wp_get_registered_image_subsizes();
-	$img_size = 'large';
+	$img_size  = 'large';
 
 	$img_details = wp_get_attachment_image_src( $thumbnail_id, $img_size );
 	$img_src     = $img_details[0];
-	$img_alt     = trim( strip_tags( get_post_meta( $thumbnail_id, '_wp_attachment_image_alt', true ) ) );
+	$img_alt     = trim( wp_strip_all_tags( get_post_meta( $thumbnail_id, '_wp_attachment_image_alt', true ) ) );
 }
 
 $article_types = array_map(
@@ -46,6 +46,7 @@ $authors = [
 		<h1 class="item-title article-item-title"><a href="<?php echo esc_attr( get_permalink( $article_id ) ); ?>"><?php echo esc_html( get_the_title( $article_id ) ); ?></a></h1>
 
 		<div class="article-teaser-byline teaser-byline">
+			<?php // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?>
 			<?php printf( '<span class="teaser-byline-by">By</span>: %s', implode( ', ', $authors ) ); ?>
 		</div>
 	</div>
