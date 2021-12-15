911ae5dcb950416a5ae7afb587d332e44dd6fba6
Boone B Gorges <boonebgorges@gmail.com>
2021-12-15 14:55:40 -0600

    Avoid PHP notices.

--- templates/teasers/article.php
+++ templates/teasers/article.php
@@ -16,12 +16,16 @@ if ( $thumbnail_id ) {
 	$img_alt     = trim( wp_strip_all_tags( get_post_meta( $thumbnail_id, '_wp_attachment_image_alt', true ) ) );
 }
 
-$article_types = array_map(
-	function( $type ) {
-		return $type->name;
-	},
-	get_the_terms( $article_id, 'ssrc_article_type' )
-);
+$article_type_terms = get_the_terms( $article_id, 'ssrc_article_type' );
+$article_types      = [];
+if ( $article_type_terms ) {
+	$article_types = array_map(
+		function( $type ) {
+			return $type->name;
+		},
+		$article_type_terms
+	);
+}
 
 $authors = [
 	'Jane Doe',
