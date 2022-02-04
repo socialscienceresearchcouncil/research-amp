e618d222b91e4a62855a6232a6f96534e88e1ec5
Boone B Gorges <boonebgorges@gmail.com>
2022-02-04 14:33:24 -0600

    More consistent loading of front-end styles.

--- assets/src/frontend.js
+++ assets/src/frontend.js
@@ -1,1 +1,13 @@
+/**
+ * Shared block styles.
+ */
+import '../css/blocks.css'
+
+import './blocks/article-teasers/block.scss';
+import './blocks/article-teasers-with-featured-article/block.scss';
+import './blocks/citation-teasers/block.scss';
+import './blocks/news-item-teasers/block.scss';
+import './blocks/research-review-teasers/block.scss';
 import './blocks/research-topic-teasers/block.scss';
+
+import './blocks/homepage-slides/block.scss';
