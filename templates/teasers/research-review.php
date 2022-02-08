b3356a4dab36d5757972c25023630d32c34d246d
Boone B Gorges <boonebgorges@gmail.com>
2022-02-08 14:27:14 -0600

    Use ramp-thumbnail image size when building Research Review teasers.

--- templates/teasers/research-review.php
+++ templates/teasers/research-review.php
@@ -6,7 +6,7 @@ $img_alt      = '';
 $thumbnail_id = get_post_thumbnail_id( $research_review_id );
 if ( $thumbnail_id ) {
 	$all_sizes = wp_get_registered_image_subsizes();
-	$img_size  = 'large';
+	$img_size  = 'ramp-thumbnail';
 
 	$img_details = wp_get_attachment_image_src( $thumbnail_id, $img_size );
 	$img_src     = $img_details[0];
@@ -21,13 +21,15 @@ if ( $img_src ) {
 ?>
 
 <article class="<?php echo esc_attr( $article_class ); ?>">
-	<?php if ( $img_src ) : ?>
-		<div class="teaser-thumb research-review-teaser-thumb">
+	<div class="teaser-thumb research-review-teaser-thumb">
+		<?php if ( $img_src ) : ?>
 			<a href="<?php echo esc_attr( get_permalink( $research_review_id ) ); ?>">
 				<img class="research_review-teaser-thumb-img" src="<?php echo esc_attr( $img_src ); ?>" alt="<?php echo esc_attr( $img_alt ); ?>" />
 			</a>
-		</div>
-	<?php endif; ?>
+		<?php else : ?>
+			&nbsp; <?php /* Force flex to use the available space */ ?>
+		<?php endif; ?>
+	</div>
 
 	<div class="teaser-content research-review-teaser-content">
 		<h1 class="item-title research-review-item-title"><a href="<?php echo esc_attr( get_permalink( $research_review_id ) ); ?>"><?php echo esc_html( get_the_title( $research_review_id ) ); ?></a></h1>
