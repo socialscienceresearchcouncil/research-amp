27e888735301a23ff1b6fecce5615512d177529d
Boone B Gorges <boonebgorges@gmail.com>
2022-02-15 14:11:24 -0600

    Standardization of the loading of frontend block stylesheets.
    Don't use "style". Do enqueue frontend.css.

--- assets/src/frontend.js
+++ assets/src/frontend.js
@@ -3,13 +3,12 @@
  */
 import '../css/blocks.css'
 
-/*
 import './blocks/article-teasers/block.scss';
 import './blocks/citation-teasers/block.scss';
+import './blocks/event-teasers/block.scss';
 import './blocks/news-item-teasers/block.scss';
 import './blocks/profile-teasers/block.scss';
 import './blocks/research-review-teasers/block.scss';
 import './blocks/research-topic-teasers/block.scss';
 
 import './blocks/homepage-slides/block.scss';
-*/
