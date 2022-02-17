3e143fb9eb110019edcb18e8ab81b8982bbf0651
Boone B Gorges <boonebgorges@gmail.com>
2022-02-17 15:05:48 -0600

    Citation Library: Support for passing 'research-topic' as URL param.

--- templates/blocks/citation-teasers.php
+++ templates/blocks/citation-teasers.php
@@ -4,50 +4,67 @@ $research_topic_id = \SSRC\RAMP\Blocks::get_research_topic_from_template_args( $
 
 $number_of_items = isset( $args['numberOfItems'] ) ? (int) $args['numberOfItems'] : 3;
 
-$show_filters = ! empty( $args['showFilters'] );
-if ( $show_filters ) {
-	$rt_map = ramp_app()->get_cpttax_map( 'research_topic' );
-
-	$current_rt = isset( $_GET['research-topic'] ) ? wp_unslash( $_GET['research-topic'] ) : null;
-
-	if ( $current_rt ) {
-		$current_rt_term = get_term_by( 'slug', $current_rt, 'ramp_topic' );
-		// translators: research topic name
-		$current_message = sprintf( __( 'Recently added citations for: "%s"', 'ramp' ), $current_rt_term->name );
-	} else {
-		$current_message = __( 'Recently added citations for "All Research Topics"', 'ramp' );
-	}
-}
-
 $query_args = [
 	'post_type'      => 'ramp_citation',
 	'post_status'    => 'publish',
 	'posts_per_page' => $number_of_items,
 	'fields'         => 'ids',
+	'tax_query'      => [],
 ];
+
+$requested_rt = isset( $_GET['research-topic'] ) ? wp_unslash( $_GET['research-topic'] ) : null;
+
+$research_topics = get_posts(
+	[
+		'post_type'      => 'ramp_topic',
+		'posts_per_page' => -1,
+		'orderby'        => [ 'title' => 'ASC' ],
+	]
+);
+
+if ( ! in_array( $requested_rt, wp_list_pluck( $research_topics, 'slug' ), true ) ) {
+	$requested_rt = null;
+}
+
+$requested_rt_term = null;
+if ( $requested_rt ) {
+	$requested_rt_term = get_term_by( 'slug', $requested_rt, 'ramp_assoc_topic' );
+
+	$query_args['tax_query']['assoc_topic'] = [
+		'taxonomy' => 'ramp_assoc_topic',
+		'terms'    => $requested_rt->term_id,
+		'field'    => 'term_id',
+	];
+}
+
+$show_filters = ! empty( $args['showFilters'] );
+if ( $show_filters ) {
+	$rt_map = ramp_app()->get_cpttax_map( 'research_topic' );
+
+	if ( $requested_rt_term ) {
+		// translators: research topic name
+		$current_message = sprintf( __( 'Recently added citations for: "%s"', 'ramp' ), $requested_rt_term->name );
+	} else {
+		$current_message = __( 'Recently added citations for "All Research Topics"', 'ramp' );
+	}
+}
 
 if ( $research_topic_id ) {
 	$rt_map     = ramp_app()->get_cpttax_map( 'research_topic' );
 	$rt_term_id = $rt_map->get_term_id_for_post_id( $research_topic_id );
 
-	$query_args['tax_query'] = [
-		[
-			'taxonomy' => 'ramp_assoc_topic',
-			'terms'    => $rt_term_id,
-			'field'    => 'term_id',
-		],
+	$query_args['tax_query']['assoc_topic'] = [
+		'taxonomy' => 'ramp_assoc_topic',
+		'terms'    => $rt_term_id,
+		'field'    => 'term_id',
 	];
 }
 
-$citations = get_posts( $query_args );
-
+// phpcs:ignore WordPress.Security.NonceVerification.Recommended
 $toggle_class = empty( $_GET['research-topic'] ) ? 'toggle-closed' : '';
 
-$research_topics = get_posts( [
-	'post_type'      => 'ramp_topic',
-	'posts_per_page' => -1,
-	'orderby'        => [ 'title' => 'ASC' ],
-] );
+$citations = get_posts( $query_args );
+
 ?>
 
 <?php if ( $show_filters ) : ?>
@@ -56,7 +73,7 @@ $research_topics = get_posts( [
 	wp_enqueue_script( 'ramp-directory-filters' );
 	?>
 
-	<div class="directory-filters-container no-js <?php echo $toggle_class; ?>">
+	<div class="directory-filters-container no-js <?php echo esc_attr( $toggle_class ); ?>">
 		<div class="directory-filter-toggle">
 			<button id="directory-filter-toggle-button" class="filter-toggle" aria-expanded="false" aria-controls="directory-filters">
 				<h3><?php esc_html_e( 'Filter Citations', 'ramp' ); ?></h3>
@@ -68,7 +85,7 @@ $research_topics = get_posts( [
 				<div class="directory-filter">
 					<label for="research-topic"><?php esc_html_e( 'Filter Citations:', 'ramp' ); ?></label>
 					<select id="research-topic" class="pretty-select directory-filter-dropdown" name="research-topic" placeholder="<?php esc_attr_e( 'All Research Topics', 'ramp' ); ?>">
-						<option <?php selected( ! $current_rt ); ?> value=""><?php esc_html_e( 'All Research Topics', 'ramp' ); ?></option>
+						<option <?php selected( ! $requested_rt ); ?> value=""><?php esc_html_e( 'All Research Topics', 'ramp' ); ?></option>
 						<?php
 						foreach ( $research_topics as $research_topic ) {
 							$rt_term_id = $rt_map->get_term_id_for_post_id( $research_topic->ID );
@@ -77,7 +94,7 @@ $research_topics = get_posts( [
 							printf(
 								'<option value="%s" %s>%s</option>',
 								esc_attr( $rt_term->slug ),
-								selected( $rt_term->slug, $current_rt, false ),
+								selected( $rt_term->slug, $requested_rt, false ),
 								esc_html( $rt_term->name )
 							);
 						}
