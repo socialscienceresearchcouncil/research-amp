a7ee42b06e18618f39f0f88827906bc1ddbb1c9d
Boone B Gorges <boonebgorges@gmail.com>
2022-02-25 10:17:50 -0600

    Some work toward citation library filters.

--- inc/block-types/citation-library-filters.php
+++ inc/block-types/citation-library-filters.php
@@ -2,8 +2,17 @@
 
 return [
 	'api_version'     => 2,
-	'attributes'      => [],
-	'render_callback' => function( $atts ) {
+	'attributes'      => [
+		'backgroundColor' => [
+			'type' => 'string',
+		],
+		'textColor' => [
+			'type' => 'string',
+		],
+	],
+	'render_callback' => function( $atts, $content, $block ) {
+
+		_b( $block->context );
 		return ramp_render_block( 'citation-library-filters', $atts );
 	},
 ];
