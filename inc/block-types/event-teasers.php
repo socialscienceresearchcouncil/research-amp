d75ee1675d9e4225578691eaad916ae8a8093b34
Boone B Gorges <boonebgorges@gmail.com>
2022-03-24 14:30:49 -0500

    Styling and formatting for event-teasers.

--- inc/block-types/event-teasers.php
+++ inc/block-types/event-teasers.php
@@ -3,17 +3,26 @@
 return [
 	'api_version'     => 2,
 	'attributes'      => [
-		'isEditMode'    => [
+		'contentMode'                => [
+			'type'    => 'string',
+			'enum'    => [ 'auto', 'all', 'advanced' ],
+			'default' => 'auto',
+		],
+		'contentModeProfileId'       => [
+			'type'    => 'integer',
+			'default' => 0,
+		],
+		'contentModeResearchTopicId' => [
+			'type'    => 'integer',
+			'default' => 0,
+		],
+		'isEditMode'                 => [
 			'type'    => 'boolean',
 			'default' => false,
 		],
-		'numberOfItems' => [
-			'type'    => 'string',
-			'default' => '3',
-		],
-		'researchTopic' => [
-			'type'    => 'string',
-			'default' => 'auto',
+		'numberOfItems'              => [
+			'type'    => 'integer',
+			'default' => 3,
 		],
 	],
 	'render_callback' => function( $atts ) {
