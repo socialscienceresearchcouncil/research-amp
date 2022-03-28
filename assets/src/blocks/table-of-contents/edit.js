405685cc2fccb8d5a72d9cbdd098874e839c0c19
Boone B Gorges <boonebgorges@gmail.com>
2022-03-28 13:48:06 -0500

    Show preview of fake TOC in table-of-contents edit mode.

--- assets/src/blocks/table-of-contents/edit.js
+++ assets/src/blocks/table-of-contents/edit.js
@@ -35,6 +35,40 @@ export default function edit( {
 
 	const headingTextValue = headingText ?? __( 'Version', 'ramp' )
 
+	const fakeTOC = (
+		<div id="ez-toc-container" className="ez-toc-v2_0_17 counter-hierarchy ez-toc-grey">
+			<nav>
+				<ul className="ez-toc-list ez-toc-list-level-1">
+					<li className="ez-toc-page-1 ez-toc-heading-level-2">
+						<span className="ez-toc-link ez-toc-heading-1">Introduction</span>
+					</li>
+
+					<li className="ez-toc-page-1 ez-toc-heading-level-2">
+						<span className="ez-toc-link ez-toc-heading-1">Some background on the project</span>
+					</li>
+
+					<li className="ez-toc-page-1 ez-toc-heading-level-2">
+						<span className="ez-toc-link ez-toc-heading-1">An overview of the literature</span>
+
+						<ul className="ez-toc-list-level-3">
+							<li className="ez-toc-heading-level-3">
+								<span className="ez-toc-link ez-toc-heading-6">United States</span>
+							</li>
+
+							<li className="ez-toc-heading-level-3">
+								<span className="ez-toc-link ez-toc-heading-6">Europe</span>
+							</li>
+						</ul>
+					</li>
+
+					<li className="ez-toc-page-1 ez-toc-heading-level-2">
+						<span className="ez-toc-link ez-toc-heading-1">Conclusion</span>
+					</li>
+				</ul>
+			</nav>
+		</div>
+	)
+
 	return (
 		<>
 			<div { ...blockProps }>
@@ -44,6 +78,8 @@ export default function edit( {
 					tagName="h3"
 					value={ headingTextValue }
 				/>
+
+				{ fakeTOC }
 			</div>
 		</>
 	)
