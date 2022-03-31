fe4c76c8e9e9780856766a55fe32f66901504d8e
Boone B Gorges <boonebgorges@gmail.com>
2022-03-31 15:56:26 -0500

    Support for contentMode and showRowRules in citation-teasers.

--- inc/block-types/citation-teasers.php
+++ inc/block-types/citation-teasers.php
@@ -3,17 +3,30 @@
 return [
 	'api_version'     => 2,
 	'attributes'      => [
-		'isEditMode'    => [
+		'contentMode'                => [
+			'type'    => 'string',
+			'enum'    => [ 'auto', 'all', 'advanced' ],
+			'default' => 'auto',
+		],
+		'contentModeProfileId'       => [
+			'type'    => 'integer',
+			'default' => 0,
+		],
+		'contentModeResearchTopicId' => [
+			'type'    => 'integer',
+			'default' => 0,
+		],
+		'isEditMode'                 => [
 			'type'    => 'boolean',
 			'default' => false,
 		],
-		'numberOfItems' => [
+		'numberOfItems'              => [
 			'type'    => 'number',
 			'default' => 3,
 		],
-		'researchTopic' => [
-			'type'    => 'string',
-			'default' => 'auto',
+		'showRowRules'               => [
+			'type'    => 'boolean',
+			'default' => false,
 		],
 	],
 	'render_callback' => function( $atts ) {
--- templates/blocks/citation-teasers.php
+++ templates/blocks/citation-teasers.php
@@ -1,55 +1,63 @@
 <?php
 
-$research_topic_id = \SSRC\RAMP\Blocks::get_research_topic_from_template_args( $args );
+$r = array_merge(
+	[
+		'contentMode'                => 'auto',
+		'contentModeResearchTopicId' => 0,
+		'contentModeProfileId'       => 0,
+		'isEditMode'                 => false,
+		'numberOfItems'              => 3,
+		'showRowRules'               => true,
+	],
+	$args
+);
 
-$number_of_items = isset( $args['numberOfItems'] ) ? (int) $args['numberOfItems'] : 3;
+$number_of_items = (int) $r['numberOfItems'];
 
 $query_args = [
 	'post_type'      => 'ramp_citation',
 	'post_status'    => 'publish',
 	'posts_per_page' => $number_of_items,
 	'fields'         => 'ids',
-	'tax_query'      => [],
+	'tax_query'      => \SSRC\RAMP\Blocks::get_content_mode_tax_query_from_template_args( $r ),
 ];
 
-// phpcs:ignore WordPress.Security.NonceVerification.Recommended
-$requested_rt = isset( $_GET['research-topic'] ) ? wp_unslash( $_GET['research-topic'] ) : null;
+// phpcs:disable WordPress.Security.NonceVerification.Recommended
+$requested_topic  = isset( $_GET['research-topic'] ) ? wp_unslash( $_GET['research-topic'] ) : null;
+$requested_search = isset( $_GET['search-term'] ) ? wp_unslash( $_GET['search-term'] ) : null;
+// phpcs:enable WordPress.Security.NonceVerification.Recommended
 
-$research_topics = get_posts(
-	[
-		'post_type'      => 'ramp_topic',
-		'posts_per_page' => -1,
-		'orderby'        => [ 'title' => 'ASC' ],
-	]
-);
-
-if ( ! in_array( $requested_rt, wp_list_pluck( $research_topics, 'post_name' ), true ) ) {
-	$requested_rt = null;
-}
-
-$requested_rt_term = null;
-if ( $requested_rt ) {
-	$requested_rt_term = get_term_by( 'slug', $requested_rt, 'ramp_assoc_topic' );
+if ( $requested_topic ) {
+	$requested_topic_term = get_term_by( 'slug', $requested_topic, 'ramp_assoc_topic' );
 
 	$query_args['tax_query']['assoc_topic'] = [
 		'taxonomy' => 'ramp_assoc_topic',
-		'terms'    => $requested_rt_term->term_id,
+		'terms'    => $requested_topic_term->term_id,
 		'field'    => 'term_id',
 	];
 }
 
-// phpcs:ignore WordPress.Security.NonceVerification.Recommended
-$requested_search_term = isset( $_GET['search-term'] ) ? wp_unslash( $_GET['search-term'] ) : null;
-if ( $requested_search_term ) {
-	$query_args['s'] = $requested_search_term;
+if ( $requested_search ) {
+	$query_args['s'] = $requested_search;
 }
 
 $citations = get_posts( $query_args );
 
+$list_classes = [
+	'item-type-list',
+	'item-type-list-citations',
+];
+
+if ( $r['showRowRules'] ) {
+	$list_classes[] = 'has-row-rules';
+} else {
+	$list_classes[] = 'has-no-row-rules';
+}
+
 ?>
 
 <div class="citation-teasers">
-	<ul class="item-type-list item-type-list-citations">
+	<ul class="<?php echo esc_attr( implode( ' ', $list_classes ) ); ?>">
 		<?php foreach ( $citations as $citation ) : ?>
 			<li>
 				<?php ramp_get_template_part( 'teasers/citation', [ 'id' => $citation ] ); ?>
