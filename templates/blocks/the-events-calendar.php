94c64baaff24c47eacd3eb1233d723599ff9ca1e
Boone B Gorges <boonebgorges@gmail.com>
2022-04-01 16:55:35 -0500

    Styling for events search bar.

--- templates/blocks/the-events-calendar.php
+++ templates/blocks/the-events-calendar.php
@@ -2,5 +2,11 @@
 
 use Tribe\Events\Views\V2\Template_Bootstrap;
 
-// phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
-echo tribe( Template_Bootstrap::class )->get_view_html();
+?>
+
+<div class="wp-block-ramp-the-events-calendar">
+	<?php
+	// phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
+	echo tribe( Template_Bootstrap::class )->get_view_html();
+	?>
+</div>
