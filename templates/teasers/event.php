46a1a5fb0e44a1f317632a2d987cfe38c993db50
Boone B Gorges <boonebgorges@gmail.com>
2022-04-06 06:47:38 -0500

    Adapt event teaser template for search results.

--- templates/teasers/event.php
+++ templates/teasers/event.php
@@ -1,5 +1,16 @@
 <?php
-$event_id = $args['id'];
+
+$r = array_merge(
+	[
+		'id'                   => 0,
+		'is_edit_mode'         => false,
+		'show_item_type_label' => false,
+		'show_research_topics' => false,
+	],
+	$args
+);
+
+$event_id = $r['id'];
 
 $start_date = tribe_get_start_date( $event_id, false, 'F j, Y' );
 $end_date   = tribe_get_end_date( $event_id, false, 'F j, Y' );
@@ -9,13 +20,17 @@ if ( $start_date === $end_date ) {
 	$event_date = sprintf( '%s - %s', $start_date, $end_date );
 }
 
-$is_edit_mode = ! empty( $args['is_edit_mode'] );
+$is_edit_mode = $r['is_edit_mode'];
 
 $article_classes = [ 'teaser' ];
 
 ?>
 
 <article class="<?php echo esc_attr( implode( ' ', $article_classes ) ); ?>">
+	<?php if ( $r['show_item_type_label'] ) : ?>
+		<?php ramp_get_template_part( 'item-type-label', [ 'label' => __( 'Citation', 'ramp' ) ] ); ?>
+	<?php endif; ?>
+
 	<div class="teaser-content event-teaser-content">
 		<div class="event-teaser-icon">
 			<img role="presentation" src="<?php echo esc_url( RAMP_PLUGIN_URL . '/assets/img/event.svg' ); ?>" />
@@ -35,6 +50,18 @@ $article_classes = [ 'teaser' ];
 			</h3>
 
 			<div class="event-meta"><?php echo esc_html( $event_date ); ?></div>
+
+			<?php if ( $r['show_research_topics'] ) : ?>
+				<?php
+				ramp_get_template_part(
+					'research-topic-tags',
+					[
+						'is_edit_mode' => $is_edit_mode,
+						'item_id'      => $event_id,
+					]
+				);
+				?>
+			<?php endif; ?>
 		</div>
 	</div>
 </article>
