9f03dcbcef5b190ddabd9288af3b73c2eae44a10
Boone B Gorges <boonebgorges@gmail.com>
2022-04-13 09:51:33 -0500

    Fix href for research topic tags.

--- templates/research-topic-tag.php
+++ templates/research-topic-tag.php
@@ -1,4 +1,8 @@
 <?php
+
+$rt_map     = ramp_app()->get_cpttax_map( 'research_topic' );
+$rt_post_id = $rt_map->get_post_id_for_term_id( $args['term_id'] );
+
 $research_topic_tag = get_term( $args['term_id'] );
 
 $is_edit_mode = ! empty( $args['is_edit_mode'] );
@@ -15,5 +19,5 @@ if ( ! empty( $args['display_type'] ) && 'plain' === $args['display_type'] ) {
 <?php if ( $is_edit_mode ) : ?>
 	<span class="<?php echo esc_attr( implode( ' ', $tag_classes ) ); ?>"><?php echo esc_html( $research_topic_tag->name ); ?></span>
 <?php else : ?>
-	<a class="<?php echo esc_attr( implode( ' ', $tag_classes ) ); ?>" href="<?php echo esc_attr( get_term_link( $research_topic_tag ) ); ?>"><?php echo esc_html( $research_topic_tag->name ); ?></a>
+	<a class="<?php echo esc_attr( implode( ' ', $tag_classes ) ); ?>" href="<?php echo esc_attr( get_permalink( $rt_post_id ) ); ?>"><?php echo esc_html( $research_topic_tag->name ); ?></a>
 <?php endif; ?>
