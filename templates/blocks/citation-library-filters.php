c074201f3b87b081c792b20c7b51e1a07e8efcb5
Boone B Gorges <boonebgorges@gmail.com>
2022-04-28 11:54:23 -0500

    Improve mobile styling for directory filters.

--- templates/blocks/citation-library-filters.php
+++ templates/blocks/citation-library-filters.php
@@ -5,23 +5,15 @@ wp_enqueue_script( 'ramp-directory-filters' );
 
 ?>
 
-<div class="directory-filters-container">
-	<div class="directory-filter-toggle">
-		<button id="directory-filter-toggle-button" class="filter-toggle" aria-expanded="false" aria-controls="directory-filters">
-			<h3><?php esc_html_e( 'Filter Citations', 'ramp' ); ?></h3>
-		</button>
-	</div>
+<div role="region" class="directory-filters">
+	<form method="get" class="directory-filter-form" action="">
+		<?php ramp_get_template_part( 'filters/search' ); ?>
 
-	<div id="directory-filters" role="region" aria-labelledby="directory-filter-toggle-button" class="directory-filters clearfix">
-		<form method="get" class="directory-filter-form" action="">
-			<?php ramp_get_template_part( 'filters/search' ); ?>
+		<div class="filter-by-legend">
+			<?php esc_html_e( 'Filter by:', 'ramp' ); ?>
+		</div>
 
-			<div class="filter-by-legend">
-				<?php esc_html_e( 'Filter by:', 'ramp' ); ?>
-			</div>
-
-			<?php ramp_get_template_part( 'filters/research-topic' ); ?>
-			<?php ramp_get_template_part( 'filters/submit' ); ?>
-		</form>
-	</div>
+		<?php ramp_get_template_part( 'filters/research-topic' ); ?>
+		<?php ramp_get_template_part( 'filters/submit' ); ?>
+	</form>
 </div>
