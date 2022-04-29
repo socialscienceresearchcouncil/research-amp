c7859ed8cfc8539e6b056b715e80f1162c6ee381
Boone B Gorges <boonebgorges@gmail.com>
2022-04-29 10:52:11 -0500

    Remove unused tools from Profile class.

--- src/Profile.php
+++ src/Profile.php
@@ -15,7 +15,6 @@ class Profile {
 		'title'         => '',
 		'homepage_url'  => '',
 		'is_featured'   => false,
-		'is_advisory'   => false,
 	];
 
 	protected $meta_keys = [
@@ -55,9 +54,6 @@ class Profile {
 
 		$is_featured               = get_post_meta( $post->ID, 'is_featured', true );
 		$this->data['is_featured'] = '1' === $is_featured;
-
-		$is_advisory               = get_post_meta( $post->ID, 'is_advisory', true );
-		$this->data['is_advisory'] = '1' === $is_advisory;
 	}
 
 	public function save() {
@@ -66,7 +62,6 @@ class Profile {
 		}
 
 		update_post_meta( $this->data['post_id'], 'is_featured', (int) $this->get_is_featured() );
-		update_post_meta( $this->data['post_id'], 'is_advisory', (int) $this->get_is_advisory() );
 
 		$post_vars = [
 			'ID'           => $this->get_post_id(),
@@ -138,10 +133,6 @@ class Profile {
 
 	public function get_is_featured() {
 		return (bool) $this->data['is_featured'];
-	}
-
-	public function get_is_advisory() {
-		return (bool) $this->data['is_advisory'];
 	}
 
 	public function get_focus_tags() {
@@ -226,71 +217,9 @@ class Profile {
 		return $img_src;
 	}
 
-	public function get_avatar_markup() {
-		$url    = $this->get_avatar_url();
-		$class  = '';
-		$markup = '';
-
-		if ( $url ) {
-			$markup = sprintf(
-				'<div class="profile-avatar" style="background-image:url(\'%s\');"></div>',
-				esc_attr( $url )
-			);
-		} else {
-			// @todo Need a dynamic way to define default image.
-			$url   = get_stylesheet_directory_uri() . '/assets/MediaWell-logo-white.svg';
-			$class = 'profile-avatar-default';
-
-			$markup = sprintf(
-				'<div class="profile-avatar profile-avatar-default" style="background-image:url(\'%s\');"></div>',
-				$url
-			);
-		}
-
-		return $markup;
-	}
-
 	public function get_associated_user_id() {
 		$id = get_post_meta( $this->get_post_id(), 'associated_user', true );
 		return (int) $id;
-	}
-
-	public function provision_wp_user( $args ) {
-		$r = array_merge(
-			[
-				'email'    => '',
-				'username' => '',
-				'category' => '',
-			],
-			$args
-		);
-
-		if ( empty( $r['email'] ) || empty( $r['username'] ) ) {
-			return false;
-		}
-
-		$GLOBALS['mediawell_is_provisioned_user'] = true;
-		$user_id                                  = register_new_user( $r['username'], $r['email'] );
-		unset( $GLOBALS['mediawell_is_provisioned_user'] );
-
-		if ( is_wp_error( $user_id ) ) {
-			return $user_id;
-		}
-
-		wp_update_user(
-			[
-				'ID'           => $user_id,
-				'display_name' => $this->get_display_name(),
-			]
-		);
-
-		if ( $r['category'] ) {
-			update_user_meta( $user_id, 'user_category', $r['category'] );
-		}
-
-		update_post_meta( $this->get_post_id(), 'associated_user', $user_id );
-
-		return $user_id;
 	}
 
 	/**
@@ -308,49 +237,6 @@ class Profile {
 				'posts_per_page' => -1,
 				'fields'         => 'ids',
 			]
-		);
-
-		return array_map( 'intval', $posts );
-	}
-
-	/**
-	 * Fetches an array of the IDs of advisory board profiles.
-	 *
-	 * @return array
-	 */
-	public static function get_advisory_ids() {
-		$posts = get_posts(
-			[
-				'post_type'      => 'ramp_profile',
-				'post_status'    => 'publish',
-				'meta_key'       => 'is_advisory',
-				'meta_value'     => '1',
-				'posts_per_page' => -1,
-				'fields'         => 'ids',
-			]
-		);
-
-		// Order by last name.
-		usort(
-			$posts,
-			function( $a_id, $b_id ) {
-				$a = self::get_instance( $a_id );
-				$b = self::get_instance( $b_id );
-
-				if ( $a->get_last_name() > $b->get_last_name() ) {
-					return 1;
-				} elseif ( $a->get_last_name() < $b->get_last_name() ) {
-					return -1;
-				} else {
-					if ( $a->get_first_name() > $b->get_first_name() ) {
-						return 1;
-					} elseif ( $a->get_first_name() < $b->get_first_name() ) {
-						return -1;
-					}
-				}
-
-				return 0;
-			}
 		);
 
 		return array_map( 'intval', $posts );
