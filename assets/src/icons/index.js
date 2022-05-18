34bbd547cefad614d4875788ccf50660b4db389e
Boone B Gorges <boonebgorges@gmail.com>
2022-05-18 10:11:07 -0500

    cite-this icon for cite-this block.

--- assets/src/icons/index.js
+++ assets/src/icons/index.js
@@ -1,4 +1,5 @@
 export { icon as articleTeasers } from './library/article-teasers'
 export { icon as changelog } from './library/changelog'
 export { icon as citation } from './library/citation'
+export { icon as citeThis } from './library/cite-this'
 export { icon as filter } from './library/filter'
