7dbc75e0c0644ff11ac8b6b1c538a616719806b3
Boone B Gorges <boonebgorges@gmail.com>
2022-05-18 09:31:47 -0500

    article-teasers block icon

--- assets/src/blocks/article-teasers/index.js
+++ assets/src/blocks/article-teasers/index.js
@@ -1,27 +1,22 @@
 /**
- * Research Topics block.
+ * Article Teasers block.
  */
 
-import { __ } from '@wordpress/i18n';
-import { registerBlockType } from '@wordpress/blocks';
+import { __ } from '@wordpress/i18n'
+import { registerBlockType } from '@wordpress/blocks'
 
 /**
  * Internal dependencies
  */
-import edit from './edit';
-import metadata from './block.json';
+import edit from './edit'
+import metadata from './block.json'
+import { icon } from './icon'
 
 /**
  * Block definition.
  */
 registerBlockType( metadata, {
-	/**
-	 * @see ./edit.js
-	 */
+	icon, 
 	edit,
-
-	/**
-	 * Rendered in PHP.
-	 */
 	save: () => { return null },
-} );
+} )
