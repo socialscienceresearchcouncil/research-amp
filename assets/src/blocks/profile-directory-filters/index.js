c1516591b97a61e47bea33c5ff561441698e0505
Boone B Gorges <boonebgorges@gmail.com>
2022-05-18 10:03:37 -0500

    filter icon for profile-directory-filters, citation-library-filters.

--- assets/src/blocks/profile-directory-filters/index.js
+++ assets/src/blocks/profile-directory-filters/index.js
@@ -9,11 +9,13 @@ import { registerBlockType } from '@wordpress/blocks'
  */
 import edit from './edit'
 import metadata from './block.json'
+import { filter as icon } from '../../icons'
 
 /**
  * Block definition.
  */
 registerBlockType( metadata, {
+	icon,
 	edit,
 	metadata,
 	save: () => { return null },
--- assets/src/icons/index.js
+++ assets/src/icons/index.js
@@ -1,3 +1,4 @@
 export { icon as articleTeasers } from './library/article-teasers'
 export { icon as changelog } from './library/changelog'
 export { icon as citation } from './library/citation'
+export { icon as filter } from './library/filter'
