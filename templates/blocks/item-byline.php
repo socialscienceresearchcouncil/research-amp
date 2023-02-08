e3b0789fd21ced92ea44e0e6764974a77ad220e9
Boone B Gorges <boonebgorges@gmail.com>
2023-02-08 17:42:36 -0600

    Use PressForward data to build item-byline strings on ramp_news_item posts.
    Fixes #23.

--- templates/blocks/item-byline.php
+++ templates/blocks/item-byline.php
@@ -6,8 +6,24 @@ if ( ! isset( $args['block']->context['postId'] ) ) {
 
 $item_id = $args['block']->context['postId'];
 
-$author_links  = \SSRC\RAMP\Profile::get_profile_links_for_post( $item_id );
-$author_string = implode( ', ', $author_links );
+if ( 'ramp_news_item' === get_post_type( $item_id ) && function_exists( 'pressforward' ) ) {
+	$date_string   = '';
+	$author_string = pressforward( 'controller.metas' )->retrieve_meta( get_the_ID(), 'item_author' );
+
+	if ( 'Author on Source' === $author_string ) {
+		$author_string = '';
+	}
+
+	$publication_date = get_post_meta( get_queried_object_id(), 'publication_date', true );
+	if ( $publication_date ) {
+		$date_string = gmdate( 'F j, Y', strtotime( $publication_date ) );
+	}
+} else {
+	$author_links  = \SSRC\RAMP\Profile::get_profile_links_for_post( $item_id );
+	$author_string = implode( ', ', $author_links );
+
+	$date_string = get_the_date( '', $item_id );
+}
 
 $byline_args = [];
 if ( $author_string ) {
@@ -15,7 +31,7 @@ if ( $author_string ) {
 }
 
 if ( ! empty( $args['showPublicationDate'] ) ) {
-	$byline_args['date'] = get_the_date( '', $item_id );
+	$byline_args['date'] = $date_string;
 }
 
 ?>
