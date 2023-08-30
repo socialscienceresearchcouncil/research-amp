8002e4408cf4b9c3608cee12c1b0785ebe8eadb3
Boone B Gorges <boonebgorges@gmail.com>
2023-08-30 09:56:01 -0500

    eslint for assets/src/icons

--- assets/src/icons/List.js
+++ assets/src/icons/List.js
@@ -1,7 +1,7 @@
-import { Path, SVG } from '@wordpress/primitives'
+import { Path, SVG } from '@wordpress/primitives';
 
 export const ListIcon = () => (
 	<SVG className="ramp-icon" height="24" viewBox="0 0 24 24" width="24">
-		<Path d="M3 13h2v-2H3v2zm0 4h2v-2H3v2zm0-8h2V7H3v2zm4 4h14v-2H7v2zm0 4h14v-2H7v2zM7 7v2h14V7H7zm-4 6h2v-2H3v2zm0 4h2v-2H3v2zm0-8h2V7H3v2zm4 4h14v-2H7v2zm0 4h14v-2H7v2zM7 7v2h14V7H7z"/>
+		<Path d="M3 13h2v-2H3v2zm0 4h2v-2H3v2zm0-8h2V7H3v2zm4 4h14v-2H7v2zm0 4h14v-2H7v2zM7 7v2h14V7H7zm-4 6h2v-2H3v2zm0 4h2v-2H3v2zm0-8h2V7H3v2zm4 4h14v-2H7v2zm0 4h14v-2H7v2zM7 7v2h14V7H7z" />
 	</SVG>
-)
+);
--- assets/src/icons/library/homepage-slides.js
+++ assets/src/icons/library/homepage-slides.js
@@ -1,7 +1,7 @@
-import { Path, SVG } from '@wordpress/primitives'
+import { Path, SVG } from '@wordpress/primitives';
 
 export const icon = (
 	<SVG xmlns="http://www.w3.org/2000/svg" height="24" width="24">
-		<Path d="M2 7H6V17H2ZM7 5H17V19H7ZM9 7V17ZM18 7H22V17H18ZM9 7V17H15V7Z"/>
+		<Path d="M2 7H6V17H2ZM7 5H17V19H7ZM9 7V17ZM18 7H22V17H18ZM9 7V17H15V7Z" />
 	</SVG>
-)
+);
